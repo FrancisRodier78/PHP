490e8566fba9697c05c7b6157ebf8e5ab920bc9a
FrancisRodier78 <FrancisRodier78@users.noreply.github.com>
2020-03-01 15:22:43 +0100

    Rajout d'un array pour la prise en compte des caratères en UTF8 sinon apparition de ???.

--- Divers/PDOFactory.php
+++ Divers/PDOFactory.php
@@ -7,7 +7,7 @@ class PDOFactory
 {
   public static function getMysqlConnexion()
   {
-    $db = new \PDO('mysql:host=localhost;dbname=news', 'root', '');
+    $db = new \PDO('mysql:host=localhost;dbname=news', 'root', '', array(\PDO::MYSQL_ATTR_INIT_COMMAND => 'SET NAMES \'UTF8\''));
     $db->setAttribute(\PDO::ATTR_ERRMODE, \PDO::ERRMODE_EXCEPTION);
  
     return $db;
